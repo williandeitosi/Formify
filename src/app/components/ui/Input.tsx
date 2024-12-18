5ce1c5f1f8ae45befc47baa7d08c743b7259fcf5
williandeitosi <williandeitosi@gmail.com>
2024-12-17 21:43:26 -0300

    feat: create Input

---
+++ src/app/components/ui/Input.tsx
@@ -0,0 +1,19 @@
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  label: string;
+  error?: string;
+}
+
+export default function Input({ label, error, ...props }: InputProps) {
+  return (
+    <div>
+      <label className="block text-sm font-medium text-gray-700 mb-1">
+        {label}
+      </label>
+      <input
+        {...props}
+        className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+      />
+      {error && <span className="text-xs text-red-800">{error}</span>}
+    </div>
+  );
+}

538d9a2c2f968d514a5cc739c29349255e00dd1b
williandeitosi <williandeitosi@gmail.com>
2024-12-17 21:44:51 -0300

    refactor: update LoginForm and RegisterForm to use reusable Input and Button components

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,10 +1,8 @@
 "use client";
 
-import { useLoginForm } from "@/hooks/useLoginForm";
+import LoginForm from "@/app/components/forms/LoginForm";
 import Link from "next/link";
 export default function LoginPage() {
-  const { errors, handleSubmit, register } = useLoginForm();
-
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col justify-center py-12 sm:px-6 lg:px-8 text-black">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
@@ -15,68 +13,7 @@ export default function LoginPage() {
 
       <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
         <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
-          <form
-            onSubmit={handleSubmit}
-            className="space-y-6"
-            action="#"
-            method="POST"
-          >
-            <div>
-              <label
-                htmlFor="email"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Email
-              </label>
-              <div className="mt-1">
-                <input
-                  {...register("email")}
-                  id="email"
-                  type="email"
-                  autoComplete="email"
-                  className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                />
-                {errors.email && (
-                  <span className="text-xs text-red-800">
-                    {errors.email?.message}
-                  </span>
-                )}
-              </div>
-            </div>
-
-            <div>
-              <label
-                htmlFor="password"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Senha
-              </label>
-              <div className="mt-1">
-                <input
-                  {...register("password")}
-                  id="password"
-                  type="password"
-                  autoComplete="current-password"
-                  className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                />
-                {errors.password && (
-                  <span className="text-xs text-red-800">
-                    {errors.password?.message}
-                  </span>
-                )}
-              </div>
-            </div>
-
-            <div>
-              <button
-                type="submit"
-                className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-              >
-                Entrar
-              </button>
-            </div>
-          </form>
-
+          <LoginForm />
           <div className="mt-6">
             <div className="relative">
               <div className="absolute inset-0 flex items-center">
--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,83 +1,18 @@
 "use client";
 
+import RegisterForm from "@/app/components/forms/RegisterForm";
 import Link from "next/link";
-import { useRegisterForm } from "../../../hooks/useRegisterForm";
 export default function RegisterPage() {
-  const { errors, handleSubmit, register, registerUser, errorMessage } =
-    useRegisterForm();
-
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col justify-center py-12 sm:px-6 lg:px-8 text-black">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
         <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
           Crie sua conta
         </h2>
-        {errorMessage && <div className="error-message">{errorMessage}</div>}
       </div>
       <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
         <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
-          <form
-            onSubmit={handleSubmit(registerUser)}
-            className="space-y-6"
-            action="#"
-            method="POST"
-          >
-            <div>
-              <label
-                htmlFor="email"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Email
-              </label>
-              <div className="mt-1">
-                <input
-                  {...register("email")}
-                  id="email"
-                  type="email"
-                  autoComplete="email"
-                  className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                />
-                {errors && (
-                  <span className="text-xs text-red-700">
-                    {errors.email?.message}
-                  </span>
-                )}
-              </div>
-            </div>
-
-            <div>
-              <label
-                htmlFor="password"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Senha
-              </label>
-              <div className="mt-1">
-                <input
-                  {...register("password")}
-                  id="password"
-                  type="password"
-                  autoComplete="new-password"
-                  className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                />
-                {errors && (
-                  <span className="text-xs text-red-800">
-                    {errors.password?.message}
-                  </span>
-                )}
-              </div>
-            </div>
-
-            <div>
-              <button
-                type="submit"
-                className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-              >
-                Registrar
-              </button>
-            </div>
-          </form>
-
+          <RegisterForm />
           <div className="mt-6">
             <div className="relative">
               <div className="absolute inset-0 flex items-center">
@@ -89,7 +24,6 @@ export default function RegisterPage() {
                 </span>
               </div>
             </div>
-
             <div className="mt-6">
               <Link
                 href="/login"

bac5cbe754c7ae344320e481af41809c19531174
williandeitosi <williandeitosi@gmail.com>
2024-12-17 21:43:36 -0300

    feat: create Button

---
+++ src/app/components/ui/Button.tsx
@@ -0,0 +1,14 @@
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  children: React.ReactNode;
+}
+
+export default function Button({ children, ...props }: ButtonProps) {
+  return (
+    <button
+      {...props}
+      className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+    >
+      {children}
+    </button>
+  );
+}

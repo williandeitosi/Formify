b916da22e629192d22fa1290a99de9fdccf91d38
williandeitosi <williandeitosi@gmail.com>
2024-12-17 22:25:17 -0300

    feat: add confirm password validation

--- src/types/registerForm.ts
+++ src/types/registerForm.ts
@@ -1,12 +1,18 @@
 import { z } from "zod";
 
-export const registerUserFormScheme = z.object({
-  email: z
-    .string()
-    .min(1, "E-maill é obrigatório")
-    .email("Formato de E-mail invalido")
-    .toLowerCase(),
-  password: z.coerce.string().min(6, "minimo 6 caracteres"),
-});
+export const registerUserFormScheme = z
+  .object({
+    email: z
+      .string()
+      .min(1, "E-maill é obrigatório")
+      .email("Formato de E-mail invalido")
+      .toLowerCase(),
+    password: z.coerce.string().min(6, "minimo 6 caracteres"),
+    confirmPassword: z.coerce.string().min(6, "minimo 6 caracteres"),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    path: ["confirmPassword"],
+    message: "As senhas estão diferentes",
+  });
 
 export type registerUserFormData = z.infer<typeof registerUserFormScheme>;
